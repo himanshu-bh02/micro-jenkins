d90be65e787e2fbd1c3992eb89a29d9800e566f7
Himanshu Bhansali <himanshu-bh02>
2023-02-02 18:01:03 +0530

    first commit

---
+++ src/main/java/com/micro/jenkins/MicroJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.micro.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroJenkinsApplication.class, args);
+	}
+
+}
